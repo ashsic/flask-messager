f972fa6ef4f77447db6a25c0707a089c86594591
ashsic <99445200+ashsic@users.noreply.github.com>
2024-03-18 21:15:43 -0400

    can now create group chats

--- flaskr/conversation.py
+++ flaskr/conversation.py
@@ -13,39 +13,77 @@ def index():
     db = get_db()
     conversations = db.execute(
         'SELECT * FROM user'
-
-        # 'SELECT p.id, title, body, created, author_id, username'
-        # ' FROM post p JOIN user u ON p.author_id = u.id'
-        # ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('conversation/index.html', conversations=conversations)
+    messages = db.execute(
+        'SELECT * FROM user_message'
+    ).fetchall()
+    return render_template('conversation/index.html', conversations=conversations, messages=messages)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
-        username = request.form['username'].split(" ")
+        usernames = request.form['username'].split(" ")
         body = request.form['body']
         error = None
 
-        if not username:
+        if not usernames:
             error = 'username is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
-            db.execute(
-                'INSERT INTO app_conversation (is_group)'
-                ' VALUES (?)'
-                'INSERT INTO user_conversation_membership'
-                ' VALUES (?, ?)'
-                'INSERT INTO user_message'
+            # Group chats are always created (barring future limitations)
+            if len(usernames) > 1:
+                db.execute(
+                    'INSERT INTO app_conversation (is_group)'
+                    ' VALUES (?)',
+                    (True,)
+                )
+                conv_id = db.execute(
+                    'SELECT id FROM app_conversation ORDER BY id DESC LIMIT 1;'
+                ).fetchone()[0]
+                usernames.append(g.user['username'])
+                for user in usernames:
+                    user_id = db.execute(
+                        'SELECT id FROM user WHERE username = ?',
+                        (user,)
+                    ).fetchone()[0]
+                    db.execute(
+                        'INSERT INTO user_conversation_membership'
+                        ' VALUES (?, ?)',
+                        (conv_id, user_id)   
+                    )
+                
+                db.execute(
+                    'INSERT INTO user_message (sender_id, conversation_id, body)'
+                    ' VALUES (?, ?, ?)'
+                    ,
+                    (g.user['id'], conv_id, body)
+                )
+            # else:
+            #     # check for existing conversation between 2 users
+            #     conv_id = db.execute(
+            #         'SELECT id FROM app_conversation ac'
+            #         ' INNER JOIN (SELECT * FROM user_conversation_membership WHERE )'
+            #         ' WHERE is_group = FALSE'
+            #     )
+            
+            # Happens if group chat, or no existing dm:
+            # usernames.append(g.user['username'])
+            # for user in usernames:
+            #     user_id = db.execute(
+            #         'SELECT id FROM user WHERE username = ?',
+            #         (user)
+            #     )
+            #     db.execute(
+            #         'INSERT INTO user_conversation_membership'
+            #         ' VALUES (?, ?)',
+            #         (conv_id, user_id)   
+            #     )
 
-                ,
-                (len(username) > 1, g.user['id'], username)
-            )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('conversation.index'))
 
-    return render_template('blog/create.html')
+    return render_template('conversation/create.html')
